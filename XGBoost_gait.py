c2f6f9e9d05ba1f4ac9f16e194ccea5f15990a99
Ohm-T <tom.salembien@telecom-sudparis.eu>
2022-05-21 14:39:50 +0200

    Comparaison differentes méthodes + XGBoost server IPP

--- XGBoost_gait.py
+++ XGBoost_gait.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[2]:
 
 
 import numpy as np 
@@ -13,12 +13,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 #LABIA
-#source1 = '../data/Gait_Data___Arm_swing.csv'
-#source2 = '../data/Consensus_Committee_Analytic_Datasets_28OCT21.xlsx'
+source1 = '../data/Gait_Data___Arm_swing.csv'
+source2 = '../data/Consensus_Committee_Analytic_Datasets_28OCT21.xlsx'
 
 #Local jupyter
-source2 = "../Quick_Start/Consensus_Committee_Analytic_Datasets_28OCT21.xlsx"
-source1 = "../Motor___MDS-UPDRS/Gait_Data___Arm_swing.csv"
+#source2 = "../Quick_Start/Consensus_Committee_Analytic_Datasets_28OCT21.xlsx"
+#source1 = "../Motor___MDS-UPDRS/Gait_Data___Arm_swing.csv"
 
 
 # import data for a first visualisation
@@ -49,10 +49,16 @@ df_complete = df_first.loc[df_na_arm['PATNO'],['Cohort']].replace(['Prodromal',"
 #Replace Nan in df_arm['COHORT']
 df_arm.loc[df_arm.COHORT.isnull(), 'COHORT'] = list(df_complete['Cohort'])
 
-df_arm.head(50)
+# remplace v8 et V8 par V08 idem V2 v6 V10 v10 v11
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='v8')]='V08'
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='V8')]='V08'
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='V2')]='V02'
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='v6')]='V06'
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='v10')]='V10'
+df_arm["EVENT_ID"].iloc[np.where(df_arm["EVENT_ID"]=='v11')]='V11'
 
 
-# In[7]:
+# In[ ]:
 
 
 # 1ère méthode pour combler Nan avec KNN
@@ -85,25 +91,6 @@ df_arm_full.head(40)
 # 
 # ---
 
-# In[8]:
-
-
-import xgboost as xgb
-from sklearn import metrics
-from pprint import pprint
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-
-x_train, x_test, y_train, y_test = train_test_split(df_arm_full, df_arm['COHORT'], test_size=0.2)
-xgbc = xgb.XGBClassifier()
-pipeline = Pipeline([('scaler', StandardScaler()), ('classifieur', xgbc)])
-xgbc.fit(x_train, y_train)
-print('Base Parameters utilisés :')
-pprint(xgbc.get_params())
-
-
 # In[10]:
 
 
@@ -128,14 +115,31 @@ def comp(model, x_test, y_test, title):
     print("Rapport :\n{}".format(metrics.classification_report(y_test, pred)))
 
 
+# In[8]:
+
+
+import xgboost as xgb
+from sklearn import metrics
+from pprint import pprint
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardSca
+from sklearn import model_selection as ms
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+
+x_train, x_test, y_train, y_test = train_test_split(df_arm_full, df_arm['COHORT'], test_size=0.2)
+xgbc = xgb.XGBClassifier()
+pipeline = Pipeline([('scaler', StandardScaler()), ('classifieur', xgbc)])
+xgbc.fit(x_train, y_train)
+comp(xgbc, x_test, y_test, 'Base XGBC')
+
+
 # In[11]:
 
 
-from sklearn import model_selection as ms
-comp(xgbc, x_test, y_test, 'Base XGBC')
-kf = ms.KFold(n_splits=5, shuffle=True)
-kf_scores = ms.cross_val_score(xgbc, x_train, y_train, cv=kf)
-print("K-fold Cross Validation vaut en moyenne: {:.2f}".format(kf_scores.mean()))
+
+# kf = ms.KFold(n_splits=5, shuffle=True)
+#kf_scores = ms.cross_val_score(xgbc, x_train, y_train, cv=kf)
+#print("K-fold Cross Validation vaut en moyenne: {:.2f}".format(kf_scores.mean()))
 
 
 # ---
@@ -143,7 +147,7 @@ print("K-fold Cross Validation vaut en moyenne: {:.2f}".format(kf_scores.mean())
 # 
 # ---
 
-# In[12]:
+# In[14]:
 
 
 # 1er balayage sur large éventail de sets d'hyperparamètres avec RandomizedSearchCV
@@ -162,13 +166,13 @@ grille_xgb = {
 # Choix aléatoire parmi 4*4*9*1*3*4*4*4*4=110592 combinaisons de paramètres possibles
 
 
-# In[13]:
+# In[15]:
 
 
 # Random Search CV training
 
 # 500 combinaisons et K = 5 folds
-xgbc_random = RandomizedSearchCV(estimator = xgbc, param_distributions = grille_xgb, n_iter = 50, cv = 5, n_jobs = -1)
+xgbc_random = RandomizedSearchCV(estimator = xgbc, param_distributions = grille_xgb, n_iter = 500, cv = 5, n_jobs = -1)
 xgbc_random.fit(x_train, y_train)
 xgbc_random.best_params_
 
@@ -182,14 +186,6 @@ xgbc_random.best_params_
 #  'lambda': 1,
 #  'gamma': 1,
 #  'colsample_bytree': 0.1}
-
-# **Remarques:**
-# - *Analyse :*           
-#   Pour lr = 0.01 et max_depth=100 et num_iter=5000 classification moyenne avec accuracy moyenne de 58%  
-#   
-# - *Solutions :*  
-#   Changer la mesure sur laquelle nous effectuons une classification car 0.44 de corrélation,   
-#   c'était attendu que la classification ne soit pas bonne / se baser sur plus de mesures ?/ imputation des Nan nécessaire ?
 
 # In[ ]:
 
